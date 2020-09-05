c8d7d4932bc8d8303d2e934d936731f8c11737a9
Tomking1982-Qi <69493150+Tomking1982-Qi@users.noreply.github.com>
2020-09-05 20:21:55 +0800

    Create 26NumIslands.java

---
+++ Week_04/26NumIslands.java
@@ -0,0 +1,37 @@
+class Solution {
+    private int m;
+    private int n;
+    private int result = 0;
+
+    public int numIslands(char[][] grid) {
+        if (grid.length == 0 || grid[0].length == 0 ) {
+            return result;
+        }
+        m = grid.length;
+        n = grid[0].length;
+        for( int i = 0; i < m ; i++ ) {
+            for (int j = 0; j < n ; j++) {
+                if (grid[i][j] == '0') {
+                    continue;
+                }
+                dfs(grid, i , j);
+                result ++;
+            }
+        }
+        return result;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= m || j >= n ) {
+            return;
+        }
+        if (grid[i][j] == '0') {
+            return;
+        }
+        grid[i][j] = '0';
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j -1 );
+        dfs(grid, i, j +1 );
+    }
+}
