58b8bbcd16102e9f783fcdcd854a5846294c0fe9
Tomking1982-Qi <69493150+Tomking1982-Qi@users.noreply.github.com>
2020-10-19 00:08:03 +0800

    Create 70MinWindow.java

---
+++ Week_09/70MinWindow.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        int[] window = new int[128], need = new int[128];
+        char[] ss = s.toCharArray(), tt = t.toCharArray();
+        int count = 0, min = ss.length;
+        String res = "";
+        for (int i = 0; i < tt.length; i++) {
+            need[tt[i]]++;
+        }
+        int i = 0, j = 0;
+        while(j < ss.length) {
+            char c = ss[j];
+            window[c]++;
+            if (window[c] <= need[c]) count++;
+            while(count == tt.length) {
+                if (j - i + 1 <= min){
+                    res = s.substring(i, j + 1);
+                    min = j - i + 1;
+                }
+                window[ss[i]]--;
+                if (window[ss[i]] < need[ss[i]]) count--;
+                i++;
+                if (i >= ss.length) break;
+            }
+            j++;
+        }
+        return res;
+    }
+}
